39b8314802edb6589c9ede5ce1d9024a72837297
GataullinaLili <127747080+GataullinaLili@users.noreply.github.com>
2026-05-13 10:46:15 +0300

    Create ItemStorageApplication.java

---
+++ src/main/java/com/itemstorage/ItemStorageApplication.java
@@ -0,0 +1,11 @@
+package com.itemstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemStorageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ItemStorageApplication.class, args);
+    }
+}

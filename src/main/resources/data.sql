b373774b6556a0e7f2e433edc6b9705f031e08ce
GataullinaLili <127747080+GataullinaLili@users.noreply.github.com>
2026-05-13 12:13:59 +0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,6 +4,12 @@ INSERT INTO users (login, password_hash, full_name, role, active, created_at) VA
 ('SKLAD', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'Петров П.П.', 'STOREKEEPER', true, NOW()),
 ('PRIEM', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'Сидорова С.С.', 'RECEPTIONIST', true, NOW());
 
+-- Тестовые пациенты
+INSERT INTO patients (medical_card_number, full_name, birth_date, created_at) VALUES
+('ИБ-10001', 'Иванов Иван Иванович', '1980-03-20', NOW()),
+('ИБ-10002', 'Петров Петр Петрович', '1975-07-15', NOW()),
+('ИБ-10003', 'Сидорова Анна Сергеевна', '1990-12-05', NOW());
+
 INSERT INTO storages (name, storage_type) VALUES
 ('Склад приёмного отделения', 'RECEPTION'),
 ('Склад долговременного хранения №1', 'LONG_TERM');

5c40a60cbfb6e1051dc23a321abc4881ae7da032
GataullinaLili <127747080+GataullinaLili@users.noreply.github.com>
2026-05-13 11:54:41 +0300

    Create PatientDTO.java

---
+++ src/main/java/com/itemstorage/dto/PatientDTO.java
@@ -0,0 +1,11 @@
+package com.itemstorage.dto;
+
+import lombok.Data;
+
+@Data
+public class PatientDTO {
+    private String misId;
+    private String fullName;
+    private String birthDate;
+    private String medicalCardNumber;
+}
